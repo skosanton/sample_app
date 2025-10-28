eb13dde887027648cdfe69689da55851521f303a
Anton Skosyrev <anton.skosyrev@gmail.com>
2025-10-28 06:27:11 -0700

    changed db name to match

--- backend/main.py
+++ backend/main.py
@@ -49,7 +49,7 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, futu
 Base = declarative_base()
 
 class Item(Base):
-    __tablename__ = "demo_items"
+    __tablename__ = "backend-db"
     id = Column(Integer, primary_key=True, autoincrement=True)
     value = Column(String(255), unique=True, nullable=False)
 
